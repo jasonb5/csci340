cfb75688096baa6d5fdf16edf0f28631f52b862b
Jason Boutte <boutte.jason@gmail.com>
2014-10-26 23:53:23 -0700

    Adds documentation

--- lab4/multi-lookup.c
+++ lab4/multi-lookup.c
@@ -1,7 +1,8 @@
 #include "multi-lookup.h"
 
 int main(int argc, char **argv) {
-  if (argc < 3) {
+ 	// Check for minimum required arguments 
+	if (argc < 3) {
     error("%s <input files> <output file>", argv[0]);
     
     exit(1);
@@ -11,18 +12,33 @@ int main(int argc, char **argv) {
  	FILE *fp; 
 	queue queue;
   sem_t empty, full;
-  pthread_mutex_t queue_mut, file_mut;
+  pthread_mutex_t queue_mut = PTHREAD_MUTEX_INITIALIZER;
+	pthread_mutex_t file_mut = PTHREAD_MUTEX_INITIALIZER;
+
+	// Initializes queue
+  if (queue_init(&queue, QUEUE_SIZE) == QUEUE_FAILURE) {
+		error("Failed to initialize queue");
+
+		exit(1);
+	} 
  
-  queue_init(&queue, QUEUE_SIZE); 
+	// Initializes empty semaphore, allowing QUEUE_SIZE
+	// requester threads to enter critical section 
+  if (sem_init(&empty, 0, QUEUE_SIZE) == -1) {
+		error("Failed to initialize empty semaphore");
+		
+		exit(1);
+	}
 
-  pthread_mutex_init(&queue_mut, NULL);
+	// Initializes full semaphore, allowing QUEUE_SIZE
+	// resolver threads to enter critical section
+  if (sem_init(&full, 0, 0) == -1) {
+		error("Failed to initialize full semaphore");
+	
+		exit(1);
+	}
 
-  pthread_mutex_init(&file_mut, NULL);
-
-  sem_init(&empty, 0, QUEUE_SIZE);
-
-  sem_init(&full, 0, 0);
-
+	// Initialize requester resources
   int requester_count = argc - 2;
   thread_info *requesters = calloc(requester_count, sizeof(thread_info));
 
@@ -32,6 +48,7 @@ int main(int argc, char **argv) {
     exit(1);
   }
 
+	// Initializes resolver resources
   int resolver_count = get_resolver_count();
   thread_info *resolvers = calloc(resolver_count, sizeof(thread_info));
 
@@ -43,6 +60,7 @@ int main(int argc, char **argv) {
     exit(1);
   }
 
+	// Opens output file for writing
 	if ((fp = fopen(argv[(argc - 1)], "w")) == NULL) {
 		error("Failed to open output file");
 
@@ -55,7 +73,8 @@ int main(int argc, char **argv) {
   debug("Spawning %d requester threads", requester_count);
 
   debug("Spawning %d resolver threads", resolver_count);
-
+	
+	// Create single thread for each input file
   for (i = 0; i < requester_count; ++i) {
     requesters[i].id = i + 1;
 
@@ -72,6 +91,7 @@ int main(int argc, char **argv) {
     pthread_create(&requesters[i].handle, NULL, &requester_thread, &requesters[i]); 
   } 
 
+	// Create resolver threads matching cpu count
   for (i = 0; i < resolver_count; ++i) {
     resolvers[i].id = i + 1;
 
@@ -89,11 +109,14 @@ int main(int argc, char **argv) {
 
     pthread_create(&resolvers[i].handle, NULL, &resolver_thread, &resolvers[i]);
   }
- 
+
+	// Block until requesters are done, guarantees all
+	// possible input values are in the queue 
   for (i = 0; i < requester_count; ++i) {
     pthread_join(requesters[i].handle, NULL);
   }
 
+	// Queue one end value for each resolver thread
   for (i = 0; i < resolver_count; ++i) {
     sem_wait(&empty);
 
@@ -106,10 +129,12 @@ int main(int argc, char **argv) {
     sem_post(&full);
   }
 
+	// Block until all resolvers are done
   for (i = 0; i < resolver_count; ++i) {
     pthread_join(resolvers[i].handle, NULL);
   }
 
+	// Clean up resources
 	fclose(fp);
 
 	pthread_mutex_destroy(&file_mut);
@@ -125,6 +150,8 @@ int main(int argc, char **argv) {
   return 0;
 }
 
+// Opens input file, reads each value and pushes
+// into shared queue
 void *requester_thread(void *arg) {
   FILE *fp; 
   char *buffer;
@@ -138,26 +165,34 @@ void *requester_thread(void *arg) {
     return NULL;
   }
 
+	// Repeat for each line of input file
   while (1) {
+		// Allocate buffer for hostname
     buffer = malloc(MAX_NAME_LENGTH * sizeof(char));
 
     if (fscanf(fp, "%s", buffer) <= 0) {
       break;
     }
 
+		// Wait to enter critical section,
+		// guarantees space in queue
     sem_wait(thread->empty);
 
+		// Ensure mutual exclusion
     pthread_mutex_lock(thread->queue_mut);
 
     debug("Requester thread %d pushing %s", thread->id, buffer);
-
+	
+		// Push host into queue
     queue_push(thread->queue, buffer);
 
+		// Release mutex
     pthread_mutex_unlock(thread->queue_mut);
 
     sem_post(thread->full); 
   }
 
+	// Clean resources
   free(buffer);
 
   fclose(fp);
@@ -165,19 +200,26 @@ void *requester_thread(void *arg) {
   return NULL;
 }
 
+// Processes each hostname and resolves to ip
+// writes all ip values to results file
 void *resolver_thread(void *arg) {
   char *buffer;
   thread_info *thread = arg;
 
   while (1) {
+		// Wait to enter critical section
+		// Ensures something to process 
     sem_wait(thread->full);
 
+		// Ensure mutual exclusion
     pthread_mutex_lock(thread->queue_mut);
 
+		// Pop value from queue
     buffer = queue_pop(thread->queue);
 
+		// Check for end value
     if (buffer == NULL) {
-      pthread_mutex_unlock(thread->queue_mut);
+			pthread_mutex_unlock(thread->queue_mut);
 
       sem_post(thread->empty);
 
@@ -190,25 +232,34 @@ void *resolver_thread(void *arg) {
 
     pthread_mutex_unlock(thread->queue_mut);
 
+		// Increment empty semaphore freeing
+		// spaces for another host
     sem_post(thread->empty);
 
   	int i, ret; 
 		char **resolved;
 
+		// Allocate space for 20 ip's
 		resolved = malloc(20 * sizeof(char**));
 
+		// Resolves hostname
   	ret = dns_lookup(buffer, resolved);
 
+		// Ensure mutual exclusion for output file
     pthread_mutex_lock(thread->file_mut);
 
+		// Write hostname
 		fprintf(thread->out_fp, "%s", buffer);
 
+		// Write empty value if hostname fails to resolve
+		// or write all returned ip's
 		if (ret == 0) {
 			fprintf(thread->out_fp, ",");
 		} else {
 			for (i = 0; i < ret; ++i) {
 				fprintf(thread->out_fp, ",%s", resolved[i]);
-			
+				
+				// Free resources	
 				free(resolved[i]);
 			}
 		}
@@ -217,6 +268,7 @@ void *resolver_thread(void *arg) {
 
     pthread_mutex_unlock(thread->file_mut);
 
+		// Free resources
 		free(resolved);
 
     free(buffer); 
@@ -225,9 +277,11 @@ void *resolver_thread(void *arg) {
   return NULL;
 }
 
+// Returns optimal resolver threads
+// Applies min and max limits to resolver threads
 int get_resolver_count() {
   int count = MIN_RESOLVER_THREADS;
-  int cpu = sysconf(_SC_NPROCESSORS_ONLN);
+  int cpu = sysconf(_SC_NPROCESSORS_ONLN); // Queries os for cpu count
 
   if (cpu != -1 && cpu > count) {
     if (MAX_RESOLVER_THREADS != -1 && cpu > MAX_RESOLVER_THREADS) {
@@ -240,6 +294,8 @@ int get_resolver_count() {
 	return count;
 }
 
+// Returns number of ip's resolved from hostname
+// Fills result with each ip and returns the count
 int dns_lookup(const char *host, char **result) {
 	int i = 0;
 	struct addrinfo hints;
@@ -251,12 +307,14 @@ int dns_lookup(const char *host, char **result) {
 	hints.ai_flags = AI_PASSIVE;
 	hints.ai_protocol = 0;
 
+	// Resolves host to ip's
 	if (getaddrinfo(host, NULL, &hints, &results) != 0) {
 		error("Failed to resolve %s", host);	
 
 		return 0;
 	}
 
+	// Iterates through results
 	for (i = 0, rp = results; rp != NULL; rp = rp->ai_next) {
 		if (rp->ai_family == AF_INET) {	
 			result[i] = malloc(MAX_IP_LENGTH * sizeof(char));	
--- lab4/multi-lookup.h
+++ lab4/multi-lookup.h
@@ -20,8 +20,10 @@
 #include "util.h"
 #include "queue.h"
 
-#define QUEUE_SIZE 50
+// Enables debug printing
+#define DEBUG
 
+// Enables experimental IPV6 support
 //#define IPV6_SUPPORT
 
 #ifdef IPV6_SUPPORT
@@ -30,12 +32,19 @@
 #define IPV6_RESOLVE 0
 #endif
 
-#define MAX_INPUT_FILES 10
+// Sets max queue size
+#define QUEUE_SIZE 50
+// Limits maximum resolver threads
+// value of -1 set no max
 #define MAX_RESOLVER_THREADS 10
+// Limits minimum resolver threads
 #define MIN_RESOLVER_THREADS 2
+// Limits maximum hostname
 #define MAX_NAME_LENGTH 1025
+// Limits maximum IP length
 #define MAX_IP_LENGTH INET6_ADDRSTRLEN 
 
+// Structure used to pass data to threads
 typedef struct thread_info_s {
   int id;
 	FILE *out_fp;
@@ -47,11 +56,18 @@ typedef struct thread_info_s {
   pthread_mutex_t *queue_mut;
 } thread_info;
 
+// Processes input files, queues each 
+// hostname into queue
 void *requester_thread(void *arg);
+
+// Processes each hostname and writes
+// all ip's to output file
 void *resolver_thread(void *arg);
 
+// Determines number of resolver threads
 int get_resolver_count();
 
+// Resolves hostname to ip's
 int dns_lookup(const char *host, char **results);
 
 #endif // LAB4_MULTI_LOOKUP_H_
