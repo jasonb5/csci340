6584d1907a35b3764f2407cdec81e234b2dbb226
Jason Boutte <boutte.jason@gmail.com>
2014-12-02 16:12:11 -0800

    Adds lru algorithm

--- lab6/pager-lru.c
+++ lab6/pager-lru.c
@@ -15,37 +15,74 @@
 
 #include <stdio.h> 
 #include <stdlib.h>
+#include <limits.h>
 
 #include "simulator.h"
 
 void pageit(Pentry q[MAXPROCESSES]) { 
-    
-    /* This file contains the stub for an LRU pager */
-    /* You may need to add/remove/modify any part of this file */
+	/* This file contains the stub for an LRU pager */
+	/* You may need to add/remove/modify any part of this file */
 
-    /* Static vars */
-    static int initialized = 0;
-    static int tick = 1; // artificial time
-    static int timestamps[MAXPROCESSES][MAXPROCPAGES];
+	/* Static vars */
+	static int initialized = 0;
+	static int tick = 1; // artificial time
+	static int timestamps[MAXPROCESSES][MAXPROCPAGES];
 
-    /* Local vars */
-    int proctmp;
-    int pagetmp;
+	/* Local vars */
+	int x, y;
+	int lowest;
+	int lowest_index;
+	int page;
+	int proctmp;
+	int pagetmp;
 
-    /* initialize static vars on first run */
-    if(!initialized){
-	for(proctmp=0; proctmp < MAXPROCESSES; proctmp++){
+	/* initialize static vars on first run */
+	if(!initialized){
+		for(proctmp=0; proctmp < MAXPROCESSES; proctmp++){
 	    for(pagetmp=0; pagetmp < MAXPROCPAGES; pagetmp++){
-		timestamps[proctmp][pagetmp] = 0; 
+				timestamps[proctmp][pagetmp] = 0; 
 	    }
-	}
-	initialized = 1;
-    }
-    
-    /* TODO: Implement LRU Paging */
-    fprintf(stderr, "pager-lru not yet implemented. Exiting...\n");
-    exit(EXIT_FAILURE);
+		}
 
-    /* advance time for next pageit iteration */
-    tick++;
+		initialized = 1;
+	}
+
+	// Iterate through processes
+	for (x = 0; x < MAXPROCESSES; ++x) {
+		// Find each active process 
+		if (q[x].active) {
+			// Determine page
+			page = q[x].pc/PAGESIZE;
+
+			// Check if page is swapped out
+			if (!q[x].pages[page]) {
+				// Try to swap in page	
+				if (!pagein(x, page)) {
+					lowest = INT_MAX;
+					lowest_index = 0;
+	
+					// Find the page with the oldest access time
+					for (y = 0; y < q[x].npages; ++y) {
+						if (page != y && timestamps[x][y] > 0 && timestamps[x][y] < lowest) {
+							lowest = timestamps[x][y];
+			
+							lowest_index = y;
+						}				
+					}
+
+					// Try to swap out page
+					if (pageout(x, lowest_index)) {
+						// Reset timestamp for page of process
+						timestamps[x][lowest_index] = 0;
+					}
+				}	
+			} else {	
+				// Update the timestamp with latest time
+				timestamps[x][page] = tick;			
+			}
+		}
+	}   
+
+	/* advance time for next pageit iteration */
+	tick++;
 } 
